af63ee4148f057d20a56c7854e89213781a91991
Cory Fishburn <coryjr2002@gmail.com>
2024-01-19 07:43:15 -0800

    Added original starter files
    General Layout.

---
+++ src/main.js
@@ -0,0 +1,4 @@
+function loadUnits() {
+    // Implement AJAX request to load units or simply add HTML content
+    document.getElementById('unit-list').innerHTML = '<p>Unit 1: 5x5</p><p>Unit 2: 10x10</p>';
+}
